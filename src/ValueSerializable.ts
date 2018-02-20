717647e1e495404d2459d0d71b41f5bab2ee9f6f
Serabass <is_everything@mail.ru>
2018-02-20 18:12:44 +0600

    name

--- src/ValueSerializable.ts
+++ src/ValueSerializable.ts
@@ -4,8 +4,18 @@ export interface Value<T> {
     value: T;
 }
 
-export abstract class ValueSerializable<T> extends Serializable<Value<T>> {
+export class ValueSerializable<T> extends Serializable<Value<T>> {
     public constructor(public value?: T) {
         super();
+    }
+
+    public serialize(): Value<T> {
+        return {
+            value: this.value
+        }
+    }
+
+    public deserialize(data: Value<T>): void {
+        this.value = data.value;
     }
 }
